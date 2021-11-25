bc08721d754c0572aa19c35d29938be744ecf619
Igor Santos <igorsantos-silva123@outlook,com>
2021-11-25 13:48:57 -0300

    cargos async function and trycatch

--- controllers/cargos-controller.js
+++ controllers/cargos-controller.js
@@ -1,59 +1,67 @@
 const mysql = require('../mysql').pool;
 
 
-exports.getLista = (req, res, next) => {
-    mysql.getConnection((error, conn) => {
-        if (error) {
-            res.status(500).send({ error: error })
-        }
-        conn.query(
-            'SELECT * FROM cargos ',
-            (error, result, field) => {
-                if (error) {
-                    res.status(500).send({ error: error })
-                }
-
-                const response = result.map(car => {
-                    return {
-                        id_cargo: car.id_cargo,
-                        id_usuario: car.id_usuario
-
-                    }
-                });
-
-                return res.status(200).send(response)
-
+exports.getLista = async (req, res, next) => {
+    try {
+        await mysql.getConnection((error, conn) => {
+            if (error) {
+                res.status(500).send({ error: error })
             }
-        )
-        conn.end();
-    })
+            conn.query(
+                'SELECT * FROM cargos ',
+                (error, result, field) => {
+                    if (error) {
+                        res.status(500).send({ error: error })
+                    }
+
+                    const response = result.map(car => {
+                        return {
+                            id_cargo: car.id_cargo,
+                            id_usuario: car.id_usuario
+
+                        }
+                    });
+
+                    return res.status(200).send(response)
+
+                }
+            )
+            conn.end();
+        })
+    } catch (error) {
+        res.status(500).send({ error: error })
+    }
 
 };
 
-exports.getCargoIdUsuario = (req, res, next) => {
-    mysql.getConnection((error, conn) => {
-        if (error) {
-            res.status(500).send({ error: error })
-        }
-        conn.query(
-            'SELECT * FROM cargos WHERE id_usuario = ?',
-            [req.params.id_usuario],
-            (error, result, field) => {
-                if (error) {
-                    res.status(500).send({ error: error })
-                }
-
-                const response = result.map(car => {
-                    return {
-                        id_cargo: car.id_cargo,
-                        id_usuario: car.id_usuario
-                    }
-                });
-                return res.status(200).send(response)
+exports.getCargoIdUsuario = async (req, res, next) => {
+    try {
+        await mysql.getConnection((error, conn) => {
+            if (error) {
+                res.status(500).send({ error: error })
             }
-        )
-        conn.end();
-    })
+            conn.query(
+                'SELECT * FROM cargos WHERE id_usuario = ?',
+                [req.params.id_usuario],
+                (error, result, field) => {
+                    if (error) {
+                        res.status(500).send({ error: error })
+                    }
+
+                    const response = result.map(car => {
+                        return {
+                            id_cargo: car.id_cargo,
+                            id_usuario: car.id_usuario
+                        }
+                    });
+                    return res.status(200).send(response)
+                }
+            )
+            conn.end();
+        })
+    } catch (error) {
+        res.status(500).send({ error: error })
+    }
 };
 exports.getListaTipoCargo = async (req, res, next) => {
     try {
@@ -83,78 +91,90 @@ exports.getListaTipoCargo = async (req, res, next) => {
         res.status(500).send({ error: error })
     }
 };
-exports.getTipoIdCargo = (req, res, next) => {
-    mysql.getConnection((error, conn) => {
-        if (error) {
-            res.status(500).send({ error: error })
-        }
-        conn.query(
-            'SELECT * FROM cargos_tipo WHERE id_cargo = ?',
-            [req.params.id_cargo],
-            (error, result, field) => {
-                if (error) {
-                    res.status(500).send({ error: error })
-                }
-
-                const response = result.map(car => {
-                    return {
-                        id_cargo: car.id_cargo,
-                        nome_cargo: car.nome_cargo
+exports.getTipoIdCargo = async (req, res, next) => {
+    try {
+        await mysql.getConnection((error, conn) => {
+            if (error) {
+                res.status(500).send({ error: error })
+            }
+            conn.query(
+                'SELECT * FROM cargos_tipo WHERE id_cargo = ?',
+                [req.params.id_cargo],
+                (error, result, field) => {
+                    if (error) {
+                        res.status(500).send({ error: error })
                     }
-                });
-                return res.status(200).send(response)
-            }
-        )
-        conn.end();
-    })
+
+                    const response = result.map(car => {
+                        return {
+                            id_cargo: car.id_cargo,
+                            nome_cargo: car.nome_cargo
+                        }
+                    });
+                    return res.status(200).send(response)
+                }
+            )
+            conn.end();
+        })
+    } catch (error) {
+        res.status(500).send({ error: error })
+    }
 };
 
-exports.postCargoUsuario = (req, res, next) => {
-    mysql.getConnection((error, conn) => {
-        if (error) {
-            res.status(500).send({ error: error })
-        }
-        conn.query(
-            'INSERT INTO cargos(id_cargo,id_usuario)VALUES(?,?)',
-            [req.body.id_cargo, req.body.id_usuario],
-            (error, result, field) => {
-                if (error) {
-                    res.status(500).send({ error: error })
-                }
-
-                const response = {
-                    mensagem: 'Cargo Relacionado ao Usuario',
-                    id_cargo: req.body.id_cargo,
-                    id_usuario: req.body.id_usuario
-                };
-                return res.status(200).send(response);
+exports.postCargoUsuario = async (req, res, next) => {
+    try {
+        await mysql.getConnection((error, conn) => {
+            if (error) {
+                res.status(500).send({ error: error })
             }
-        )
-        conn.end();
-    })
+            conn.query(
+                'INSERT INTO cargos(id_cargo,id_usuario)VALUES(?,?)',
+                [req.body.id_cargo, req.body.id_usuario],
+                (error, result, field) => {
+                    if (error) {
+                        res.status(500).send({ error: error })
+                    }
+
+                    const response = {
+                        mensagem: 'Cargo Relacionado ao Usuario',
+                        id_cargo: req.body.id_cargo,
+                        id_usuario: req.body.id_usuario
+                    };
+                    return res.status(200).send(response);
+                }
+            )
+            conn.end();
+        })
+    } catch (error) {
+        res.status(500).send({ error: error })
+    }
 };
 
-exports.postTipoCargo = (req, res, next) => {
-    mysql.getConnection((error, conn) => {
-        if (error) {
-            res.status(500).send({ error: error })
-        }
-        conn.query(
-            'INSERT INTO cargos_tipo(id_cargo,nome_cargo)VALUES(?,?)',
-            [req.body.id_cargo, req.body.nome_cargo],
-            (error, result, field) => {
-                if (error) {
-                    res.status(500).send({ error: error })
-                }
-
-                const response = {
-                    mensagem: 'Cargo Cadastrado',
-                    id_cargo: req.body.id_cargo,
-                    nome_cargo: req.body.nome_cargo
-                };
-                return res.status(200).send(response);
+exports.postTipoCargo = async (req, res, next) => {
+    try {
+        await mysql.getConnection((error, conn) => {
+            if (error) {
+                res.status(500).send({ error: error })
             }
-        )
-        conn.end();
-    })
+            conn.query(
+                'INSERT INTO cargos_tipo(id_cargo,nome_cargo)VALUES(?,?)',
+                [req.body.id_cargo, req.body.nome_cargo],
+                (error, result, field) => {
+                    if (error) {
+                        res.status(500).send({ error: error })
+                    }
+
+                    const response = {
+                        mensagem: 'Cargo Cadastrado',
+                        id_cargo: req.body.id_cargo,
+                        nome_cargo: req.body.nome_cargo
+                    };
+                    return res.status(200).send(response);
+                }
+            )
+            conn.end();
+        })
+    } catch (error) {
+        res.status(500).send({ error: error })
+    }
 };
