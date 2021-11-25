5ebbbb352ce1ff4d14811c9620ad5b2b4cbbe45d
Igor Santos <igorsantos-silva123@outlook,com>
2021-11-25 14:15:48 -0300

    sala trycatch and async

--- controllers/sala-controller.js
+++ controllers/sala-controller.js
@@ -1,103 +1,123 @@
 const mysql = require('../mysql').pool;
 
 
-exports.getListaSala = (req, res, next) => {
-    mysql.getConnection((error, conn) => {
-        if (error) {
-            res.status(500).send({ error: error })
-        }
-        conn.query(
-            'SELECT * FROM salas',
-            (error, result, field) => {
-                if (error) {
-                    res.status(500).send({ error: error })
-                }
-
-                const response = result.map(sala => {
-                    return {
-                        id_sala: sala.id_sala,
-                        nome_sala: sala.nome_sala
+exports.getListaSala = async (req, res, next) => {
+    try {
+        await mysql.getConnection((error, conn) => {
+            if (error) {
+                res.status(500).send({ error: error })
+            }
+            conn.query(
+                'SELECT * FROM salas',
+                (error, result, field) => {
+                    if (error) {
+                        res.status(500).send({ error: error })
                     }
-                });
-                return res.status(200).send(response)
 
-            }
-        )
+                    const response = result.map(sala => {
+                        return {
+                            id_sala: sala.id_sala,
+                            nome_sala: sala.nome_sala
+                        }
+                    });
+                    return res.status(200).send(response)
 
-    })
+                }
+            )
+            conn.end();
+
+        })
+    } catch (error) {
+        res.status(500).send({ error: error })
+    }
 };
 
-exports.getIdSala = (req, res, next) => {
-    mysql.getConnection((error, conn) => {
-        if (error) {
-            res.status(500).send({ error: error })
-        }
+exports.getIdSala = async (req, res, next) => {
+    try {
+        await mysql.getConnection((error, conn) => {
+            if (error) {
+                res.status(500).send({ error: error })
+            }
 
-        conn.query(
-            'SELECT * FROM salas WHERE id_sala = ?',
-            [req.params.id_sala],
-            (error, result, field) => {
-                if (error) {
-                    res.status(500).send({ error: error })
-                }
-
-                const response = result.map(sala => {
-                    return {
-                        id_sala: sala.id_sala,
-                        nome_sala: sala.nome_sala
+            conn.query(
+                'SELECT * FROM salas WHERE id_sala = ?',
+                [req.params.id_sala],
+                (error, result, field) => {
+                    if (error) {
+                        res.status(500).send({ error: error })
                     }
-                });
-                return res.status(200).send(response)
-            }
-        )
-    })
-};
-exports.postSala = (req, res, next) => {
-    mysql.getConnection((error, conn) => {
-        if (error) {
-            res.status(500).send({ error: error })
-        }
 
-        conn.query(
-            'INSERT INTO salas(id_sala,nome_sala,imagem)VALUES(?,?,?)',
-            [req.body.id_sala, req.body.nome_sala, req.body.imagem],
-            (error, result, field) => {
-                if (error) {
-                    res.status(500).send({ error: error })
+                    const response = result.map(sala => {
+                        return {
+                            id_sala: sala.id_sala,
+                            nome_sala: sala.nome_sala
+                        }
+                    });
+                    return res.status(200).send(response)
                 }
-
-                const response = {
-                    id_sala: req.body.id_sala,
-                    nome_sala: req.body.nome_sala,
-                    imagem: req.body.imagem
-                };
-
-                return res.status(200).send(response)
-            }
-        )
-    })
+            )
+            conn.end();
+        })
+    } catch (error) {
+        res.status(500).send({ error: error })
+    }
 };
-exports.patchSala = (req, res, next) => {
-    mysql.getConnection((req, res, next) => {
-        if (error) {
-            res.status(500).send({ error: error })
-        }
-
-        conn.query(
-            'UPDATE salas SET nome_sala = ? WHERE id_sala = ?',
-            [req.body.nome_sala, req.body.id_sala],
-            (error, result, field) => {
-                if (error) {
-                    res.status(500).send({ error: error })
-                }
-
-                const response = {
-                    id_sala: req.body.id_sala,
-                    nome_sala: req.body.nome_sala
-                };
-
-                return res.status(200).send(response)
+exports.postSala = async (req, res, next) => {
+    try {
+        await mysql.getConnection((error, conn) => {
+            if (error) {
+                res.status(500).send({ error: error })
             }
-        )
-    })
+
+            conn.query(
+                'INSERT INTO salas(id_sala,nome_sala,imagem)VALUES(?,?,?)',
+                [req.body.id_sala, req.body.nome_sala, req.body.imagem],
+                (error, result, field) => {
+                    if (error) {
+                        res.status(500).send({ error: error })
+                    }
+
+                    const response = {
+                        id_sala: req.body.id_sala,
+                        nome_sala: req.body.nome_sala,
+                        imagem: req.body.imagem
+                    };
+
+                    return res.status(200).send(response)
+                }
+            )
+            conn.end();
+        })
+    } catch (error) {
+        res.status(500).send({ error: error })
+    }
+};
+exports.patchSala = async (req, res, next) => {
+    try {
+        await mysql.getConnection((error, conn) => {
+            if (error) {
+                res.status(500).send({ error: error })
+            }
+
+            conn.query(
+                'UPDATE salas SET nome_sala = ? WHERE id_sala = ?',
+                [req.body.nome_sala, req.body.id_sala],
+                (error, result, field) => {
+                    if (error) {
+                        res.status(500).send({ error: error })
+                    }
+
+                    const response = {
+                        id_sala: req.body.id_sala,
+                        nome_sala: req.body.nome_sala
+                    };
+
+                    return res.status(200).send(response)
+                }
+            )
+            conn.end()
+        })
+    } catch (error) {
+        res.status(500).send({ error: error })
+    }
 };
